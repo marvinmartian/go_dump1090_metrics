70f78a1d810b998adb6c34f441616faf5d0caa1a
Marvin <marvinmartian@gmail.com>
2022-07-25 16:02:49 -0600

    Expand struct. Basic stats.json ingest

--- src/main.go
+++ src/main.go
@@ -6,8 +6,13 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type AircraftList struct {
@@ -15,16 +20,44 @@ type AircraftList struct {
 	Aircraft []Aircraft `json:"aircraft"`
 }
 
-type Aircraft struct {
-	Hex      string `json:"hex"`
-	Flight   string `json:"flight,omitempty"`
-	AltoBaro int16  `json:"alt_baro,omitempty"`
+type Statistics struct {
+	Latest  SingleStat `json:"latest"`
+	Last_1  SingleStat `json:"last1min"`
+	Last_5  SingleStat `json:"last5min"`
+	Last_15 SingleStat `json:"last15min"`
+	Total   SingleStat `json:"total"`
 }
 
-func readFile(file string) {
+type SingleStat struct {
+	Start float32 `json:"start"`
+	End   float32 `json:"end"`
+}
+
+type Aircraft struct {
+	Hex         string  `json:"hex"`
+	Flight      string  `json:"flight,omitempty"`
+	AltoBaro    uint16  `json:"alt_baro,omitempty"`
+	GroundSpeed float32 `json:"gs,omitempty"`
+	BaroRate    int16   `json:"baro_rate,omitempty"`
+	Latitude    float32 `json:"lat,omitempty"`
+	Longitude   float32 `json:"lon,omitempty"`
+	NavHeading  float32 `json:"nav_heading,omitempty"`
+	RSSi        float32 `json:"rssi,omitempty"`
+	Category    string  `json:"category,omitempty"`
+	Emergency   string  `json:"emergency,omitempty"`
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func readAircraftFile(path string) {
 
 	// Open the file
-	jsonFile, err := os.Open(file)
+	jsonFile, err := os.Open(path + "aircraft.json")
 
 	// Print the error if that happens.
 	if err != nil {
@@ -46,17 +79,52 @@ func readFile(file string) {
 	fmt.Printf("%+v\n", aircraft_list)
 }
 
-func main() {
+func readStatsFile(path string) {
 
-	file := flag.String("file", "/run/dump1090-fa/aircraft.json", "Path to aircraft.json file")
-	flag.Parse()
-	fmt.Println(*file)
+	// Open the file
+	jsonFile, err := os.Open(path + "stats.json")
 
+	// Print the error if that happens.
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// defer file close
+	defer jsonFile.Close()
+
+	// read file
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// Initialize list of aircraft
+	var stats Statistics
+
+	// Unmarshal to aircraft list
+	json.Unmarshal(byteValue, &stats)
+
+	fmt.Printf("%+v\n", stats)
+}
+
+func readFiles(path string) {
 	ticker := time.NewTicker(5 * time.Second)
 
 	for _ = range ticker.C {
-		fmt.Println("Ticking..")
-		readFile(*file)
+		// fmt.Println("Ticking..")
+		opsProcessed.Inc()
+		readAircraftFile(path)
+		readStatsFile(path)
 	}
+
+}
+
+func main() {
+
+	path := flag.String("path", "/run/dump1090-fa/", "Path to json files. Default /run/dump1090-fa/")
+	flag.Parse()
+	fmt.Println(*path)
+
+	go readFiles(*path)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 
 }
