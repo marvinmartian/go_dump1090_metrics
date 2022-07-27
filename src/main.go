1d127b58584a9396edc7925bb99243455fd836d6
Melvin <marvinmartian@gmail.com>
2022-07-26 21:06:07 -0600

    Add prometheus metrics for aircraft.

--- src/main.go
+++ src/main.go
@@ -6,12 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -37,22 +37,84 @@ type Aircraft struct {
 	Hex         string  `json:"hex"`
 	Flight      string  `json:"flight,omitempty"`
 	AltoBaro    uint16  `json:"alt_baro,omitempty"`
-	GroundSpeed float32 `json:"gs,omitempty"`
+	AltoGeom    uint16  `json:"alt_geom,omitempty"`
+	GroundSpeed float64 `json:"gs,omitempty"`
 	BaroRate    int16   `json:"baro_rate,omitempty"`
-	Latitude    float32 `json:"lat,omitempty"`
-	Longitude   float32 `json:"lon,omitempty"`
-	NavHeading  float32 `json:"nav_heading,omitempty"`
-	RSSi        float32 `json:"rssi,omitempty"`
+	Latitude    float64 `json:"lat,omitempty"`
+	Longitude   float64 `json:"lon,omitempty"`
+	NavHeading  float64 `json:"nav_heading,omitempty"`
+	RSSi        float64 `json:"rssi,omitempty"`
 	Category    string  `json:"category,omitempty"`
 	Emergency   string  `json:"emergency,omitempty"`
+	Messages    float64 `json:"messages,omitempty"`
 }
 
+// Define the aircraft metrics
 var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
+	dump1090AltBaro = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "alt_baro",
+		Help:      "Barometric Altitude.",
+	},
+		[]string{"flight", "hex"},
+	)
+	dump1090AltGeom = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "alt_geom",
+		Help:      "Geometric Altitude.",
+	},
+		[]string{"flight", "hex"},
+	)
+	dump1090BaroRate = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "baro_rate",
+		Help:      "Rate of Barometric Change.",
+	},
+		[]string{"flight", "hex"},
+	)
+	dump1090GroundSpeed = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "gs",
+		Help:      "Ground Speed.",
+	},
+		[]string{"flight", "hex"},
+	)
+	dump1090NavHeading = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "nav_heading",
+		Help:      "Navigational Heading.",
+	},
+		[]string{"flight", "hex"},
+	)
+	dump1090Rssi = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "rssi",
+		Help:      "Signal Strength.",
+	},
+		[]string{"flight", "hex"},
+	)
+	dump1090Messages = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "messages",
+		Help:      "Number of Messages.",
+	},
+		[]string{"flight", "hex"},
+	)
 )
+
+func aircraftMetrics(aircraft []Aircraft) {
+	for _, s := range aircraft {
+		labels := prometheus.Labels{"flight": s.Flight, "hex": s.Hex}
+		dump1090AltBaro.With(labels).Set(float64(s.AltoBaro))
+		dump1090AltGeom.With(labels).Set(float64(s.AltoGeom))
+		dump1090BaroRate.With(labels).Set(float64(s.BaroRate))
+		dump1090GroundSpeed.With(labels).Set(float64(s.GroundSpeed))
+		dump1090NavHeading.With(labels).Set(float64(s.NavHeading))
+		dump1090Rssi.With(labels).Set(float64(s.RSSi))
+		dump1090Messages.With(labels).Set(float64(s.Messages))
+	}
+
+}
 
 func readAircraftFile(path string) {
 
@@ -73,10 +135,18 @@ func readAircraftFile(path string) {
 	// Initialize list of aircraft
 	var aircraft_list AircraftList
 
+	// s := fmt.Sprintf(`requests_total{file=%q}`, "aircraft")
+	// metrics.GetOrCreateCounter(s).Inc()
+
+	// metrics.GetOrCreateGauge(`dump1090_gs`, func() float64 {
+	// 	return float64(aircraft_list.Aircraft[len(aircraft_list.Aircraft)-1].GroundSpeed)
+	// })
+
 	// Unmarshal to aircraft list
 	json.Unmarshal(byteValue, &aircraft_list)
 
-	fmt.Printf("%+v\n", aircraft_list)
+	aircraftMetrics(aircraft_list.Aircraft)
+	// fmt.Printf("%+v\n", aircraft_list)
 }
 
 func readStatsFile(path string) {
@@ -98,6 +168,11 @@ func readStatsFile(path string) {
 	// Initialize list of aircraft
 	var stats Statistics
 
+	// var metricsValue uint64
+	// metricsValue = 10
+	// metricsName := fmt.Sprintf("foo=%q", "bar")
+	// metrics.GetOrCreateCounter(metricsName).Set(metricsValue)
+
 	// Unmarshal to aircraft list
 	json.Unmarshal(byteValue, &stats)
 
@@ -108,23 +183,34 @@ func readFiles(path string) {
 	ticker := time.NewTicker(5 * time.Second)
 
 	for _ = range ticker.C {
-		// fmt.Println("Ticking..")
-		opsProcessed.Inc()
 		readAircraftFile(path)
-		readStatsFile(path)
+		// readStatsFile(path)
 	}
 
+}
+
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(dump1090AltBaro)
+	prometheus.MustRegister(dump1090AltGeom)
+	prometheus.MustRegister(dump1090BaroRate)
+	prometheus.MustRegister(dump1090GroundSpeed)
+	prometheus.MustRegister(dump1090NavHeading)
+	prometheus.MustRegister(dump1090Rssi)
+	prometheus.MustRegister(dump1090Messages)
 }
 
 func main() {
 
 	path := flag.String("path", "/run/dump1090-fa/", "Path to json files. Default /run/dump1090-fa/")
+	port := flag.String("port", "3000", "Port to expose metrics")
 	flag.Parse()
-	fmt.Println(*path)
+	fmt.Println("Path to json files:", *path)
+	fmt.Println("Listen Port:", *port)
 
 	go readFiles(*path)
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 
 }
