4be9c3f5e451e724f04c9057be09e3d1528567ee
Melvin <marvinmartian@gmail.com>
2022-08-20 12:43:39 -0600

    Fix distance test

--- src/main_test.go
+++ src/main_test.go
@@ -1,10 +1,18 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestDistance(t *testing.T) {
-	distance := distance(foo, foo)
-	if distance != 10 {
+	var lat1 float64 = 50.72
+	var lon1 float64 = -113.99
+	var lat2 float64 = 51.72
+	var lon2 float64 = -115.99
+
+	distance := distance(lat1, lon1, lat2, lon2)
+
+	if distance != 110.73478240139609 {
 		t.Errorf("Distance was incorrect")
 	}
 }
