19952b14768791d871b59a1a5bacd386a5e06df8
Melvin <marvinmartian@gmail.com>
2022-08-20 12:55:18 -0600

    Improve the haversine distance calculation and test

--- src/main.go
+++ src/main.go
@@ -214,31 +214,22 @@ type requestLabels struct {
 	Hex    string `label:"hex"`
 }
 
+const radius = 6371
+
+func degrees2radians(degrees float64) float64 {
+	return degrees * math.Pi / 180
+}
+
 func distance(lat1 float64, lng1 float64, lat2 float64, lng2 float64, unit ...string) float64 {
-	radlat1 := float64(math.Pi * lat1 / 180)
-	radlat2 := float64(math.Pi * lat2 / 180)
-
-	theta := float64(lng1 - lng2)
-	radtheta := float64(math.Pi * theta / 180)
-
-	dist := math.Sin(radlat1)*math.Sin(radlat2) + math.Cos(radlat1)*math.Cos(radlat2)*math.Cos(radtheta)
-	if dist > 1 {
-		dist = 1
-	}
-
-	dist = math.Acos(dist)
-	dist = dist * 180 / math.Pi
-	dist = dist * 60 * 1.1515
-
-	if len(unit) > 0 {
-		if unit[0] == "K" {
-			dist = dist * 1.609344
-		} else if unit[0] == "N" {
-			dist = dist * 0.8684
-		}
-	}
-
-	return dist
+	degreesLat := degrees2radians(lat2 - lat1)
+	degreesLong := degrees2radians(lng2 - lng1)
+	a := (math.Sin(degreesLat/2)*math.Sin(degreesLat/2) +
+		math.Cos(degrees2radians(lat1))*
+			math.Cos(degrees2radians(lat2))*math.Sin(degreesLong/2)*
+			math.Sin(degreesLong/2))
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := radius * c
+	return d
 }
 
 func aircraftMetrics(aircraft []Aircraft) {
--- src/main_test.go
+++ src/main_test.go
@@ -10,9 +10,8 @@ func TestDistance(t *testing.T) {
 	var lat2 float64 = 51.72
 	var lon2 float64 = -115.99
 
-	distance := distance(lat1, lon1, lat2, lon2)
-
-	if distance != 110.73478240139609 {
+	distance := distance(lat1, lon1, lat2, lon2, "K")
+	if distance != 178.21893191668582 {
 		t.Errorf("Distance was incorrect")
 	}
 }
