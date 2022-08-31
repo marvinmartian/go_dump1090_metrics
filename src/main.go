cbca764fccefcded3dba162f2d500c2ea58ee3d6
Melvin <marvinmartian@gmail.com>
2022-08-31 16:12:44 -0600

    Add metrics for max range, by direction, and with mlat/position

--- src/main.go
+++ src/main.go
@@ -95,19 +95,22 @@ type StatCpr struct {
 }
 
 type Aircraft struct {
-	Hex         string  `json:"hex"`
-	Flight      string  `json:"flight,omitempty"`
-	AltoBaro    uint16  `json:"alt_baro,omitempty"`
-	AltoGeom    uint16  `json:"alt_geom,omitempty"`
-	GroundSpeed float64 `json:"gs,omitempty"`
-	BaroRate    int16   `json:"baro_rate,omitempty"`
-	Latitude    float64 `json:"lat,omitempty"`
-	Longitude   float64 `json:"lon,omitempty"`
-	NavHeading  float64 `json:"nav_heading,omitempty"`
-	RSSi        float64 `json:"rssi,omitempty"`
-	Category    string  `json:"category,omitempty"`
-	Emergency   string  `json:"emergency,omitempty"`
-	Messages    float64 `json:"messages,omitempty"`
+	Hex         string   `json:"hex"`
+	Flight      string   `json:"flight,omitempty"`
+	AltoBaro    uint16   `json:"alt_baro,omitempty"`
+	AltoGeom    uint16   `json:"alt_geom,omitempty"`
+	GroundSpeed float64  `json:"gs,omitempty"`
+	BaroRate    int16    `json:"baro_rate,omitempty"`
+	Latitude    float64  `json:"lat,omitempty"`
+	Longitude   float64  `json:"lon,omitempty"`
+	NavHeading  float64  `json:"nav_heading,omitempty"`
+	RSSi        float64  `json:"rssi,omitempty"`
+	Category    string   `json:"category,omitempty"`
+	Emergency   string   `json:"emergency,omitempty"`
+	Messages    float64  `json:"messages,omitempty"`
+	Seen        float64  `json:"seen,omitempty"`
+	SeenPos     float64  `json:"seen_pos,omitempty"`
+	Mlat        []string `json:"mlat,omitempty"`
 }
 type Coordinate struct {
 	Lat float64 `json:"lat"`
@@ -177,6 +180,41 @@ var (
 	},
 		[]string{"flight", "hex"},
 	)
+	dump1090MaxRangeDirection = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "recent_aircraft_max_range_by_direction",
+		Help:      "Max distance by direction.",
+	},
+		[]string{"direction", "time_period"},
+	)
+	dump1090CountByDirection = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "recent_aircraft_with_direction",
+		Help:      "Aircraft count by direction.",
+	},
+		[]string{"direction", "time_period"},
+	)
+	dump1090CountWithPos = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "recent_aircraft_with_position",
+		Help:      "Number of aircraft with position.",
+	},
+		[]string{"time_period"},
+	)
+	dump1090CountWithMlat = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "dump1090",
+		Name:      "recent_aircraft_with_multilateration",
+		Help:      "Number of aircraft with multilateration position.",
+	},
+		[]string{"time_period"},
+	)
+	// dump1090Observed = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	// 	Namespace: "dump1090",
+	// 	Name:      "recent_aircraft_observed",
+	// 	Help:      "Number of aircraft with multilateration position.",
+	// },
+	// 	[]string{"time_period"},
+	// )
 )
 
 var opsMetrics struct {
@@ -221,6 +259,16 @@ type requestLabels struct {
 
 const radius = 6371
 
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
 var direction_lut = [16]string{"N", "NE", "NE", "E", "E", "SE", "SE", "S", "S", "SW", "SW", "W", "W", "NW", "NW", "N"}
 
 func degrees2radians(degrees float64) float64 {
@@ -259,18 +307,55 @@ func aircraftMetrics(aircraft []Aircraft) {
 	dump1090GroundSpeed.Reset()
 	dump1090NavHeading.Reset()
 	dump1090Rssi.Reset()
+	dump1090MaxRangeDirection.Reset()
 	// dump1090Messages.Reset()
 
+	var threshold float64 = 15
+	var aircraft_observed int = 0
+	var aircraft_with_mlat int = 0
+	var aircraft_with_pos float64 = 0
+
+	aircraft_direction := make(map[string]int)
+	aircraft_direction_max_range := make(map[string]float64)
+	for d := range direction_lut {
+		aircraft_direction[direction_lut[d]] = 0
+		aircraft_direction_max_range[direction_lut[d]] = 0
+	}
+	// fmt.Println(aircraft_direction)
+	// fmt.Println(aircraft_direction_max_range)
+	// fmt.Println(threshold)
+
 	for _, s := range aircraft {
+
+		if s.Seen < threshold {
+			aircraft_observed++
+		}
+		if s.SeenPos < threshold {
+			aircraft_with_pos++
+		}
+
+		if contains(s.Mlat, "lat") {
+			aircraft_with_mlat++
+		}
+
 		labels := prometheus.Labels{"flight": strings.TrimSpace(s.Flight), "hex": s.Hex}
 		if s.Latitude != 0 {
 			dist := distance(ReceiverLat, ReceiverLon, s.Latitude, s.Longitude)
-			// angle := relative_angle(ReceiverLat, ReceiverLon, s.Latitude, s.Longitude)
-			// direction := relative_direction(angle)
+			angle := relative_angle(ReceiverLat, ReceiverLon, s.Latitude, s.Longitude)
+			direction := relative_direction(angle)
 			// fmt.Println(angle)
 			// fmt.Println(direction)
+			aircraft_direction[direction]++
+			dump1090CountByDirection.With(prometheus.Labels{"direction": direction, "time_period": "latest"}).Set(float64(aircraft_direction[direction]))
+			if dist > float64(aircraft_direction_max_range[direction]) {
+				aircraft_direction_max_range[direction] = dist
+				dump1090MaxRangeDirection.With(prometheus.Labels{"direction": direction, "time_period": "latest"}).Set(dist)
+			}
 			dump1090Distance.With(labels).Set(dist)
 		}
+
+		// details := FindAircraft(s.Hex)
+		// fmt.Println(details)
 
 		dump1090AltBaro.With(labels).Set(float64(s.AltoBaro))
 		dump1090AltGeom.With(labels).Set(float64(s.AltoGeom))
@@ -280,6 +365,15 @@ func aircraftMetrics(aircraft []Aircraft) {
 		dump1090Rssi.With(labels).Set(float64(s.RSSi))
 		// dump1090Messages.With(labels).Set(float64(s.Messages))
 	}
+
+	// fmt.Println(aircraft_direction)
+	// dump1090Observed.With(prometheus.Labels{"time_period": "latest"}).Set(float64(aircraft_observed))
+	dump1090CountWithMlat.With(prometheus.Labels{"time_period": "latest"}).Set(float64(aircraft_with_mlat))
+	dump1090CountWithPos.With(prometheus.Labels{"time_period": "latest"}).Set(float64(aircraft_with_pos))
+	// fmt.Println(aircraft_observed)
+	// fmt.Println(aircraft_with_mlat)
+	// fmt.Println(aircraft_with_pos)
+	// fmt.Println(aircraft_direction_max_range)
 
 }
 
@@ -442,6 +536,12 @@ func init() {
 	prometheus.MustRegister(dump1090Rssi)
 	prometheus.MustRegister(dump1090Messages)
 	prometheus.MustRegister(dump1090Distance)
+	prometheus.MustRegister(dump1090MaxRangeDirection)
+	prometheus.MustRegister(dump1090CountByDirection)
+	prometheus.MustRegister(dump1090CountWithPos)
+	prometheus.MustRegister(dump1090CountWithMlat)
+	// prometheus.MustRegister(dump1090Observed)
+
 }
 
 func main() {
@@ -455,6 +555,8 @@ func main() {
 	readReceiverInfo(*path)
 
 	go readFilesTicker(*path)
+
+	// go flightInit()
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
